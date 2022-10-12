3827feb60a8917768cbb6559041f4e197b87f3d6
Shatteri <elias.isopahkala@gmail.com>
2022-10-12 19:08:46 +0300

    heroku malding

--- src/main/java/com/example/eliasbookstore/EliasbookstoreApplication.java
+++ src/main/java/com/example/eliasbookstore/EliasbookstoreApplication.java
@@ -23,16 +23,16 @@ public class EliasbookstoreApplication {
 	@Bean
 	public CommandLineRunner bookStore(BookRepository brepository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
-			crepository.save(new Category("FirstCategory"));
-			crepository.save(new Category("SecondCategory"));
+			// crepository.save(new Category("FirstCategory"));
+			// crepository.save(new Category("SecondCategory"));
 
-			brepository.save(new Book("First Author", "First Title", 2001, "123456-7890", 50, crepository.findByName("FirstCategory").get(0)));
-			brepository.save(new Book("Second Author", "Second Title", 2002, "123456-7890", 50, crepository.findByName("FirstCategory").get(0)));
-			brepository.save(new Book("Third Author", "Third Title", 2003, "123456-7890", 50, crepository.findByName("FirstCategory").get(0)));
-			brepository.save(new Book("Fourth Author", "Fourth Title", 2004, "123456-7890", 50, crepository.findByName("FirstCategory").get(0)));
+			// brepository.save(new Book("First Author", "First Title", 2001, "123456-7890", 50, crepository.findByName("FirstCategory").get(0)));
+			// brepository.save(new Book("Second Author", "Second Title", 2002, "123456-7890", 50, crepository.findByName("FirstCategory").get(0)));
+			// brepository.save(new Book("Third Author", "Third Title", 2003, "123456-7890", 50, crepository.findByName("FirstCategory").get(0)));
+			// brepository.save(new Book("Fourth Author", "Fourth Title", 2004, "123456-7890", 50, crepository.findByName("FirstCategory").get(0)));
 
-			urepository.save(new User("user", "$2y$10$iyEr.tfpe9q2NrY0B9ayAeVsd4ljb5VHP8y/ioV/rFXrz2fv624yG", "USER"));
-			urepository.save(new User("admin", "$2y$10$6P79CpftG0MpMstXlAgieuIZA3fRCD89iXHc8L6zTAcTLPyxjDAse", "ADMIN"));
+			// urepository.save(new User("user", "$2y$10$iyEr.tfpe9q2NrY0B9ayAeVsd4ljb5VHP8y/ioV/rFXrz2fv624yG", "USER"));
+			// urepository.save(new User("admin", "$2y$10$6P79CpftG0MpMstXlAgieuIZA3fRCD89iXHc8L6zTAcTLPyxjDAse", "ADMIN"));
 		};
 	}
 }
